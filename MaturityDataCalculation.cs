0655af756fd2fc010f0c1956ae490f5d400951be
SureshR5 <44670626+SureshR5@users.noreply.github.com>
2018-11-02 18:14:50 +0530

    Add files via upload

---
+++ MaturityDataCalculation.cs
@@ -0,0 +1,12 @@
+﻿namespace MatuityData.Calculation.DataCalculation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class MaturityDataCalculation
+    {
+    }
+}
---
+++ MaturityDataInput.cs
@@ -0,0 +1,41 @@
+﻿namespace MatuityData.Calculation.Contracts
+{
+    using System;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MaturityDataInput
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string PolicyNumber;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime PolicyStartDate;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Premiums;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public char Membership;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string DiscretionaryBonus;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string UpliftPercentage;
+        
+    }
+}
