eb7b4b3f64129a51f5f7cd18a1bc2f9ac684316e
SureshR5 <44670626+SureshR5@users.noreply.github.com>
2018-11-02 18:01:22 +0530

    Add files via upload

---
+++ MaturityMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+
+namespace MaturityData.DataLoader
+{
+    public sealed class MaturityMap: CsvClassMap<Maturity>
+    {
+        public MaturityMap()
+        {
+            this.AutoMap();
+        }
+    }
+}
